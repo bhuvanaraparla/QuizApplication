8bacddc7903ecd98033e4b7fc2a38c6d816d48df
bhuvanaraparla <bhuvanaraparla7677@gmail.com>
2023-08-10 23:47:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+        public static String question[] ={
+                "Which is not main components in android?",
+                "Which one is not the programming language?",
+                " Which of the following applications are processed locally on the phone?",
+                "Which of the following is Apple’s cloud service for hosted email service?",
+                "Which of the following has not included support for Adobe Flash or Java on its platform?"
+        };
+
+        public static String choices[][] = {
+                {"Activity","Lifecycle","Broadcast Receiver","Content provider"},
+                {"Java","Kotlin","VScode","Python"},
+                {" Google Earth","Google Maps","Google Voice","None"},
+                {"MobileMe","MobApp","MobileCloud","MobileApp"},
+                {"Microsoft","Google","Apple","Amazon"}
+        };
+
+        public static String correctAnswers[] = {
+                "Lifecycle",
+                "VScode",
+                "None",
+                "MobileMe",
+                "Apple"
+        };
+}
